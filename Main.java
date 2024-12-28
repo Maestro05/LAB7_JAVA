ef38fb49089e1fd117138cd8004dbeaef55f0291
Nguyen Tuan Ann <nguyen.05@inbox.ru>
2024-12-28 19:11:15 +0700

    Переписывание кода и добавление нового функционала

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
-class Author implements Cloneable {
+import java.util.*;
+
+// Интерфейс для книг
+interface LibraryItem {
+    String getTitle();
+    String getAuthor();
+    int getYear();
+    void print();
+}
+
+// Класс для автора
+class Author {
     private String name;
     private String surname;
-    private String birthdate;
 
-    public Author(String name, String surname, String birthdate) {
+    public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
-        this.birthdate = birthdate;
     }
 
     public String getName() {
@@ -16,183 +25,308 @@ class Author implements Cloneable {
     public String getSurname() {
         return surname;
     }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public void print() {
-        System.out.println("Автор: " + name + " " + surname + ", Дата рождения: " + birthdate);
-    }
-
-    @Override
-    public Author clone() throws CloneNotSupportedException {
-        return (Author) super.clone(); // Мелкое клонирование
-    }
 }
 
-// Класс для категории книги
-class Category implements Cloneable {
+// Класс для категории
+class Category {
     private String name;
-    private String description;
 
-    public Category(String name, String description) {
+    public Category(String name) {
         this.name = name;
-        this.description = description;
     }
 
     public String getName() {
         return name;
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void print() {
-        System.out.println("Категория: " + name + ", Описание: " + description);
-    }
-
-    @Override
-    public Category clone() throws CloneNotSupportedException {
-        return (Category) super.clone(); // Мелкое клонирование
-    }
 }
 
 // Абстрактный класс для книги
-abstract class AbstractBook {
+abstract class Book implements LibraryItem, Comparable<Book>, Cloneable {
     protected String title;
     protected Author author;
     protected Category category;
+    protected int year;
+    protected boolean isAvailable;
+    protected boolean isReserved;
 
-    public AbstractBook(String title, Author author, Category category) {
+    public Book(String title, Author author, Category category, int year) {
         this.title = title;
         this.author = author;
         this.category = category;
-    }
-
-    public abstract void getDetails();  // Каждая книга должна предоставить детали
-}
-
-// Базовый класс для книги
-class Book extends AbstractBook implements Cloneable {
-    protected int year;
-    protected int copiesAvailable;
-
-    public Book(String title, Author author, Category category, int year, int copiesAvailable) {
-        super(title, author, category);
         this.year = year;
-        this.copiesAvailable = copiesAvailable;
+        this.isAvailable = true;
+        this.isReserved = false;
     }
 
-    public void print() {
-        System.out.println("Книга: " + title + ", Год: " + year + ", Доступных копий: " + copiesAvailable);
-        author.print();
-        category.print();
-    }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author.getName() + " " + author.getSurname(); }
+    public int getYear() { return year; }
 
     @Override
-    public void getDetails() {
-        System.out.println("Печатная книга: " + title + ", Год: " + year + ", Доступных копий: " + copiesAvailable);
+    public void print() {
+        System.out.println("Книга: " + title + ", Автор: " + getAuthor() + ", Год: " + year);
     }
 
+    // Реализация интерфейса Comparable для сортировки
+    @Override
+    public int compareTo(Book other) {
+        return this.title.compareTo(other.title); // Сортировка по названию
+    }
+
+    // Клонирование
     @Override
     public Book clone() throws CloneNotSupportedException {
-        Book clonedBook = (Book) super.clone();
-        clonedBook.author = this.author.clone();
-        clonedBook.category = this.category.clone();
-        return clonedBook; // Мелкое клонирование
+        return (Book) super.clone();
     }
 
-    // Глубокое клонирование
-    public Book deepClone() {
-        try {
-            Author clonedAuthor = this.author.clone();
-            Category clonedCategory = this.category.clone();
-            return new Book(this.title, clonedAuthor, clonedCategory, this.year, this.copiesAvailable);
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        isReserved = reserved;
     }
 }
 
-// Производный класс для электронной книги
+// Класс для электронной книги (производный класс)
 class EBook extends Book {
     private String fileFormat;
-    private double fileSize; // Размер файла в мегабайтах
+    private double fileSize;
 
-    public EBook(String title, Author author, Category category, int year, int copiesAvailable, String fileFormat, double fileSize) {
-        super(title, author, category, year, copiesAvailable);
+    public EBook(String title, Author author, Category category, int year, String fileFormat, double fileSize) {
+        super(title, author, category, year);
         this.fileFormat = fileFormat;
         this.fileSize = fileSize;
     }
 
     @Override
     public void print() {
-        super.print(); // Вызов метода базового класса
-        System.out.println("Формат файла: " + fileFormat + ", Размер файла: " + fileSize + " MB");
-    }
-
-    // Перегрузка метода print без вызова метода базового класса
-    public void printSimple() {
-        System.out.println("Электронная книга: " + title + ", Формат: " + fileFormat + ", Размер файла: " + fileSize + " MB");
-    }
-
-    @Override
-    public void getDetails() {
-        System.out.println("Электронная книга: " + title + ", Формат: " + fileFormat + ", Размер файла: " + fileSize + " MB");
-    }
-
-    @Override
-    public EBook clone() throws CloneNotSupportedException {
-        EBook clonedEBook = (EBook) super.clone();
-        return clonedEBook; // Мелкое клонирование
+        super.print();
+        System.out.println("Формат файла: " + fileFormat + ", Размер: " + fileSize + " MB");
     }
 }
 
-// Интерфейс для клонирования книг
-interface ClonableBook {
-    Book clone();  // Метод клонирования
+// Класс для библиотеки
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    // Добавление книги в библиотеку
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Книга \"" + book.getTitle() + "\" добавлена в библиотеку.");
+    }
+
+    // Поиск книги по названию
+    public Book searchByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    // Сортировка книг по названию
+    public void sortBooks() {
+        Collections.sort(books);
+        System.out.println("Книги отсортированы.");
+    }
+
+    // Резервирование книги
+    public boolean reserveBook(Book book) {
+        if (book.isAvailable() && !book.isReserved()) {
+            book.setReserved(true);
+            System.out.println("Книга \"" + book.getTitle() + "\" зарезервирована.");
+            return true;
+        }
+        System.out.println("Книга \"" + book.getTitle() + "\" недоступна для резервирования.");
+        return false;
+    }
+
+    // Выдача книги
+    public boolean lendBook(Book book) {
+        if (book.isAvailable() && !book.isReserved()) {
+            book.setAvailable(false);
+            System.out.println("Книга \"" + book.getTitle() + "\" выдана.");
+            return true;
+        }
+        System.out.println("Книга \"" + book.getTitle() + "\" недоступна для выдачи.");
+        return false;
+    }
+
+    // Возврат книги
+    public boolean returnBook(Book book) {
+        if (!book.isAvailable()) {
+            book.setAvailable(true);
+            book.setReserved(false);  // Снять резерв
+            System.out.println("Книга \"" + book.getTitle() + "\" возвращена в библиотеку.");
+            return true;
+        }
+        System.out.println("Книга \"" + book.getTitle() + "\" уже в библиотеке.");
+        return false;
+    }
+
+    // Вывод всех книг в библиотеке
+    public void printBooks() {
+        if (books.isEmpty()) {
+            System.out.println("Библиотека пуста.");
+        } else {
+            for (Book book : books) {
+                book.print();
+            }
+        }
+    }
+
+    // Вывод книг по категориям
+    public void printBooksByCategory(String categoryName) {
+        boolean found = false;
+        for (Book book : books) {
+            if (book instanceof EBook) {
+                EBook eBook = (EBook) book;
+                if (eBook.getCategory().getName().equalsIgnoreCase(categoryName)) {
+                    eBook.print();
+                    found = true;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("Нет книг в категории \"" + categoryName + "\".");
+        }
+    }
 }
 
-// Главный класс программы
+// Главный класс для тестирования программы
 public class Main {
     public static void main(String[] args) {
-        // Создание автора и категории
-        Author author = new Author("John", "Doe", "01.01.1980");
-        Category category = new Category("Programming", "Books about software development");
+        Library library = new Library();
 
-        // Создание обычной книги
-        Book originalBook = new Book("Java Programming", author, category, 2023, 10);
-        originalBook.print();
+        // Создание авторов и категорий
+        Author author1 = new Author("John", "Doe");
+        Author author2 = new Author("Jane", "Smith");
+        Category category1 = new Category("Programming");
+        Category category2 = new Category("Fiction");
 
-        // Мелкое клонирование
-        try {
-            Book shallowClonedBook = originalBook.clone();
-            System.out.println("\nМелкое клонирование:");
-            shallowClonedBook.print();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+        // Создание книг
+        Book book1 = new Book("Java Programming", author1, category1, 2023) {};
+        Book book2 = new EBook("The Hobbit", author2, category2, 1937, "EPUB", 2.3);
 
-        // Глубокое клонирование
-        Book deepClonedBook = originalBook.deepClone();
-        System.out.println("\nГлубокое клонирование:");
-        deepClonedBook.print();
+        // Добавление книг в библиотеку
+        library.addBook(book1);
+        library.addBook(book2);
 
-        // Создание электронной книги
-        EBook eBook = new EBook("Advanced Java", author, category, 2022, 5, "EPUB", 2.5);
-        System.out.println("\nЭлектронная книга (с вызовом метода базового класса):");
-        eBook.print();
+        // Меню
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("\nМеню:");
+            System.out.println("1. Добавить книгу");
+            System.out.println("2. Выдать книгу");
+            System.out.println("3. Резервировать книгу");
+            System.out.println("4. Вернуть книгу");
+            System.out.println("5. Найти книгу по названию");
+            System.out.println("6. Сортировать книги");
+            System.out.println("7. Вывести все книги");
+            System.out.println("8. Вывести книги по категории");
+            System.out.println("9. Выход");
+            System.out.print("Выберите действие: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Очистка буфера ввода
 
-        // Перегрузка метода print
-        System.out.println("\nЭлектронная книга (без вызова метода базового класса):");
-        eBook.printSimple();
+            switch (choice) {
+                case 1:
+                    // Добавление книги
+                    System.out.print("Введите название книги: ");
+                    String title = scanner.nextLine();
+                    System.out.print("Введите имя автора: ");
+                    String authorName = scanner.nextLine();
+                    System.out.print("Введите фамилию автора: ");
+                    String authorSurname = scanner.nextLine();
+                    System.out.print("Введите категорию: ");
+                    String category = scanner.nextLine();
+                    System.out.print("Введите год издания: ");
+                    int year = scanner.nextInt();
+                    library.addBook(new Book(title, new Author(authorName, authorSurname), new Category(category), year) {});
+                    break;
+                case 2:
+                    // Выдача книги
+                    System.out.print("Введите название книги для выдачи: ");
+                    String lendTitle = scanner.nextLine();
+                    Book lendBook = library.searchByTitle(lendTitle);
+                    if (lendBook != null) {
+                        library.lendBook(lendBook);
+                    } else {
+                        System.out.println("Книга не найдена.");
+                    }
+                    break;
+                case 3:
+                    // Резервирование книги
+                    System.out.print("Введите название книги для резервирования: ");
+                    String reserveTitle = scanner.nextLine();
+                    Book reserveBook = library.searchByTitle(reserveTitle);
+                    if (reserveBook != null) {
+                        library.reserveBook(reserveBook);
+                    } else {
+                        System.out.println("Книга не найдена.");
+                    }
+                    break;
+                case 4:
+                    // Возврат книги
+                    System.out.print("Введите название книги для возврата: ");
+                    String returnTitle = scanner.nextLine();
+                    Book returnBook = library.searchByTitle(returnTitle);
+                    if (returnBook != null) {
+                        library.returnBook(returnBook);
+                    } else {
+                        System.out.println("Книга не найдена.");
+                    }
+                    break;
+                case 5:
+                    // Поиск книги по названию
+                    System.out.print("Введите название книги для поиска: ");
+                    String searchTitle = scanner.nextLine();
+                    Book foundBook = library.searchByTitle(searchTitle);
+                    if (foundBook != null) {
+                        foundBook.print();
+                    } else {
+                        System.out.println("Книга не найдена.");
+                    }
+                    break;
+                case 6:
+                    // Сортировка книг
+                    library.sortBooks();
+                    break;
+                case 7:
+                    // Вывод всех книг
+                    library.printBooks();
+                    break;
+                case 8:
+                    // Вывод книг по категории
+                    System.out.print("Введите категорию для поиска книг: ");
+                    String categorySearch = scanner.nextLine();
+                    library.printBooksByCategory(categorySearch);
+                    break;
+                case 9:
+                    // Выход из программы
+                    System.out.println("Выход из программы...");
+                    break;
+                default:
+                    System.out.println("Неверный выбор. Попробуйте снова.");
+                    break;
+            }
+        } while (choice != 9);
 
-        // Демонстрация использования абстрактного класса
-        AbstractBook printedBook = new Book("Printed Java", author, category, 2021, 15);
-        System.out.println("\nАбстрактная книга:");
-        printedBook.getDetails();
+        scanner.close();
     }
 }
